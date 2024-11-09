865a1260671cbd92dc64a817dba69d9acd1f2d3f
Elio.zhang <Elio.zhang@hansonggroup.com>
2024-11-09 17:15:17 +0800

    修改README

--- example/ExamplePage/T_Graphics.cpp
+++ example/ExamplePage/T_Graphics.cpp
@@ -1,4 +1,4 @@
-#include "T_Graphics.h"
+﻿#include "T_Graphics.h"
 
 #include <QHBoxLayout>
 #include <QVBoxLayout>
@@ -12,7 +12,7 @@ T_Graphics::T_Graphics(QWidget* parent)
     // 预览窗口标题
     setWindowTitle("ElaGraphics");
     // 顶部元素
-    createCustomWidget("图形视图框架被放置于此，可在此界面体验其效果，按住Ctrl进行缩放，按住Shitf进行连接");
+    // createCustomWidget("图形视图框架被放置于此，可在此界面体验其效果，按住Ctrl进行缩放，按住Shitf进行连接");
 
     QWidget* centralWidget = new QWidget(this);
     centralWidget->setWindowTitle("ElaGraphics");

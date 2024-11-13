9cc551ac11f4b8b40762e8c467627524f068e140
Elio.zhang <Elio.zhang@hansonggroup.com>
2024-11-13 11:46:13 +0800

    新增首页表格所需要的Device类，修改表格的行列数据来源

---
+++ example/ExamplePage/Device.cpp
@@ -0,0 +1,44 @@
+﻿// Device.cpp
+#include "Device.h"
+
+// 构造函数实现
+Device::Device(const QString& name, const QString& type, const QList<QString>& subclasses)
+    : m_name(name), m_type(type), m_subclasses(subclasses)
+{
+}
+
+// 获取设备名称
+QString Device::getName() const
+{
+    return m_name;
+}
+
+// 获取设备类型
+QString Device::getType() const
+{
+    return m_type;
+}
+
+// 获取子类列表
+QList<QString> Device::getSubclasses() const
+{
+    return m_subclasses;
+}
+
+// 设置设备名称
+void Device::setName(const QString& name)
+{
+    m_name = name;
+}
+
+// 设置设备类型
+void Device::setType(const QString& type)
+{
+    m_type = type;
+}
+
+// 设置子类列表
+void Device::setSubclasses(const QList<QString>& subclasses)
+{
+    m_subclasses = subclasses;
+}
--- example/ExamplePage/Routing.h
+++ example/ExamplePage/Routing.h
@@ -4,6 +4,7 @@
 #include "ElaScrollPage.h"
 #include <QGroupBox>
 #include <QTableWidget>
+#include <QMap>  // 用于存储父级是否展开的状态
 
 class QWebEngineView;
 class ElaComboBox;
@@ -16,15 +17,20 @@ public:
     explicit Routing(QWidget* parent = nullptr);
 
 private:
-
     // 新增私有成员变量，用于过滤器和矩阵表格
-    QGroupBox* filterGroupBox{nullptr};
-    QTableWidget* matrixTable{nullptr};
+    QGroupBox* filterGroupBox{nullptr};        // 过滤器框
+    QTableWidget* matrixTable{nullptr};        // 矩阵表格
+
+    // 新增变量用于存储父级展开状态
+    QMap<int, bool>* pParentExpandedRows{nullptr};   // 存储父级行是否展开的状态
+    QMap<int, bool>* pParentExpandedColumns{nullptr}; // 存储父级列是否展开的状态
+
     // 调整表格高度使其适应内容行数的函数
     void adjustTableHeight(QTableWidget* table);
+
     // 用于初始化过滤器和矩阵表格的方法
     QGroupBox* createFilterGroup();
-    QTableWidget* createMatrixTable();
+    QTableWidget* createMatrixTable(); // 创建矩阵表格
 
 Q_SIGNALS:
 
